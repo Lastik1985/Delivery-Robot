add36fc448c2222046d1f6a958b5cea21624cb86
Lastik1985 <Lastik1985@mail.ru>
2023-01-25 22:17:29 +0300

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,27 @@
 package org.example;
 
 import java.util.*;
+
 import static java.util.Collections.max;
 
 
 class Main {
-   public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
     public static void main(String[] args) throws InterruptedException {
 
 
-    Thread[] threads = new Thread[1000];
+        Thread[] threads = new Thread[1000];
 
         for (int i = 0; i < threads.length; i++) {
-        Runnable task = () -> {
-            String route = generateRoute("RLRFR", 100);
-            int key = Math.toIntExact(route.chars().filter(n -> n == 'R').count());
-            addMap(key);
-         };
-        threads[i] = new Thread(task);
-        threads[i].start();
-    }
+            Runnable task = () -> {
+                String route = generateRoute("RLRFR", 100);
+                int key = Math.toIntExact(route.chars().filter(n -> n == 'R').count());
+                addMap(key);
+            };
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
         for (Thread thread : threads) {
             thread.join();
         }
@@ -28,9 +30,10 @@ class Main {
                 " (встретилось " + max(sizeToFreq.values()) + " раз)");
         System.out.println("Другие размеры:");
         sizeToFreq.forEach((key, value) ->
-            System.out.println(" - " + key + " " + value + " раз."));
+                System.out.println(" - " + key + " " + value + " раз."));
 
     }
+
     public static synchronized void addMap(Integer key) {
         if (sizeToFreq.containsKey(key)) {
             sizeToFreq.put(key, sizeToFreq.get(key) + 1);
@@ -39,7 +42,7 @@ class Main {
         }
     }
 
-public static String generateRoute(String letters, int length) {
+    public static String generateRoute(String letters, int length) {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
         for (int i = 0; i < length; i++) {
